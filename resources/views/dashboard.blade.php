7793b26ed63541b42eea9a250e8f890080441f39
RazerGhost <90572937+RazerGhost@users.noreply.github.com>
2023-11-04 21:08:22 +0100

    added bladewind propperly and some template for tables

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,6 +11,103 @@
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     {{ __("You're logged in!") }}
                 </div>
+                <x-bladewind.tab-group name="free-pics">
+
+                    <x-slot name="headings">
+                        <x-bladewind.tab-heading name="unsplash-1" label="Lissete Laverde" />
+
+                        <x-bladewind.tab-heading name="unsplash-2" label="Marko Pavlichenko" />
+
+                        <x-bladewind.tab-heading name="unsplash-3" active="true" label="Yoonbae Cho" />
+
+                        <x-bladewind.tab-heading name="unsplash-4" label="Sam Carter" />
+                    </x-slot>
+
+                    <x-bladewind.tab-body>
+
+                        <x-bladewind.tab-content name="unsplash-1">
+                            <x-bladewind.table divider="thin">
+                                <x-slot name="header">
+                                    <th>Name</th>
+                                    <th>Department</th>
+                                    <th>Email</th>
+                                </x-slot>
+                                <tr>
+                                    <td>Alfred Rowe</td>
+                                    <td>Outsourcing</td>
+                                    <td>alfred@therowe.com</td>
+                                </tr>
+                                <tr>
+                                    <td>Michael K. Ocansey</td>
+                                    <td>Tech</td>
+                                    <td>kabutey@gmail.com</td>
+                                </tr>
+                            </x-bladewind.table>
+                        </x-bladewind.tab-content>
+
+                        <x-bladewind.tab-content name="unsplash-2">
+                            <x-bladewind.table divider="thin">
+                                <x-slot name="header">
+                                    <th>Name</th>
+                                    <th>Department</th>
+                                    <th>Email</th>
+                                </x-slot>
+                                <tr>
+                                    <td>Alfred Rowe</td>
+                                    <td>Outsourcing</td>
+                                    <td>alfred@therowe.com</td>
+                                </tr>
+                                <tr>
+                                    <td>Michael K. Ocansey</td>
+                                    <td>Tech</td>
+                                    <td>kabutey@gmail.com</td>
+                                </tr>
+                            </x-bladewind.table>
+                        </x-bladewind.tab-content>
+
+                        <x-bladewind.tab-content name="unsplash-3" active="true">
+                            <x-bladewind.table divider="thin">
+                                <x-slot name="header">
+                                    <th>Name</th>
+                                    <th>Department</th>
+                                    <th>Email</th>
+                                </x-slot>
+                                <tr>
+                                    <td>Alfred Rowe</td>
+                                    <td>Outsourcing</td>
+                                    <td>alfred@therowe.com</td>
+                                </tr>
+                                <tr>
+                                    <td>Michael K. Ocansey</td>
+                                    <td>Tech</td>
+                                    <td>kabutey@gmail.com</td>
+                                </tr>
+                            </x-bladewind.table>
+                        </x-bladewind.tab-content>
+
+                        <x-bladewind.tab-content name="unsplash-4">
+                            <x-bladewind.table divider="thin">
+                                <x-slot name="header">
+                                    <th>Name</th>
+                                    <th>Department</th>
+                                    <th>Email</th>
+                                </x-slot>
+                                <tr>
+                                    <td>Alfred Rowe</td>
+                                    <td>Outsourcing</td>
+                                    <td>alfred@therowe.com</td>
+                                </tr>
+                                <tr>
+                                    <td>Michael K. Ocansey</td>
+                                    <td>Tech</td>
+                                    <td>kabutey@gmail.com</td>
+                                </tr>
+                            </x-bladewind.table>
+                        </x-bladewind.tab-content>
+
+                    </x-bladewind.tab-body>
+
+                </x-bladewind.tab-group>
             </div>
         </div>
     </div>
